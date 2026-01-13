7e0b0602cba628cc8b7190945a52bb05c40cdc72
VoidDeveloper67 <juniortype123@gmail.com>
2026-01-13 07:18:36 +0000

    Update SAB Spawner.lua

--- SAB Spawner.lua
+++ SAB Spawner.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/3OFXG0a1AFjRcqn8/raw", true))()
+loadstring(game:HttpGet("https://api.getpolsec.com/scripts/hosted/2eb07c95b50e8da9f36c118fada824af9b198eefa191ca45092370aee9288d33.lua", true))()
